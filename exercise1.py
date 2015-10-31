c8234cd0f3f2ccf11299021cbb7ce201785d8b42
Brady Williamson <brady.williamson@gmail.com>
2015-10-31 15:38:57 -0400

    Got exercise 1 running, though the argument going in doesn't work unless you put double quotes around it

--- exercise1.py
+++ exercise1.py
@@ -6,6 +6,8 @@ This module converts English words to Pig Latin words
 
 """
 
+
+
 __author__ = 'Susan Sim'
 __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
@@ -14,13 +16,26 @@ __license__ = "MIT License"
 
 def pig_latinify(word):
     """
-    Describe your function
+    Translate an english word into piglatin.
 
-    :param :
-    :return:
+    :param word:  a word
+    :return:  the resulting word in piglatin
     :raises:
 
     """
-    result = ""
+    word = str(word)
+    vowels = "aeiou"
+    prefix = ""
+    if word[0] in vowels:
+        result = word + "yay"
 
+    else:
+        while word[0] not in vowels:
+            prefix += word[0]
+            word = word[1:len(word)]
+        result = word + prefix + "ay"
+
+    print result
     return result
+
+pig_latinify("stew")
